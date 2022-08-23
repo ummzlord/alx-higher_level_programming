29ebc383325a542d1b5ab5ceabcc3c881a2f1666
Munachi Umeh <munachiumeh@yahoo.com>
2022-08-23 00:49:14 +0000

    dealing with strings

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print("{}{}{}\n{}".format(str, str, str, str[0:9]))#!/usr/bin/python3
+str = "Holberton School"
+print("{}{}{}\n{}".format(str, str, str, str[0:9]))
