dff894de19507520ed891bc796eceeed5ca1d63c
Munachi Umeh <munachiumeh@yahoo.com>
2022-08-29 16:14:01 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
         print("{} argument:".format(num - 1))
     else:
         print("{} arguments:".format(num - 1))
+
     for i in range(1, num):
         print("{}: {}".format(i, sys.argv[i]))
