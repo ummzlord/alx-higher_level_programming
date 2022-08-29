954d1a074f1f2429b41377f7a8159e57899b85f3
Munachi Brian Umeh <61619731+ummzlord@users.noreply.github.com>
2022-08-29 16:57:34 +0100

    let's test 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
         print("{} argument:".format(num - 1))
     else:
         print("{} arguments:".format(num - 1))
-
     for i in range(1, num):
         print("{}: {}".format(i, sys.argv[i]))
