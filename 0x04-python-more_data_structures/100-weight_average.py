0c670aa3b1bc56126440f45e52d5b5d1f22bc812
Munachi Umeh <munachiumeh@yahoo.com>
2022-09-02 16:31:08 -0700

    git instrucution re-read and redid

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    average = 0
+    div = 0
+    for tup in my_list:
+        average += tup[0] * tup[1]
+        div += tup[1]
+    return float(average / div)
